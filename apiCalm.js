e4fb5ce025c9d80bd32c0bb715c8dbcc1ab3f63e
Luciano Delgado <ldelgado@grupolpa.com>
2023-08-17 15:22:18 -0300

    port

--- apiCalm.js
+++ apiCalm.js
@@ -50,7 +50,7 @@ app.post("/send-email", (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Servidor iniciado en el puerto ${PORT}`);
 });
